b0fbbd0a344d92ea10ac05a7054ecda677be5cb2
Alextinto <alexander.correa.ortega@gmail.com>
2023-11-22 23:58:15 -0300

    Toolbar, splash empty, and color palette

--- app/src/main/java/com/acorrea/inventoried/HomeActivity.kt
+++ app/src/main/java/com/acorrea/inventoried/HomeActivity.kt
@@ -3,32 +3,15 @@ package com.acorrea.inventoried
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.widget.Toolbar
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
-        var inventoryButton = findViewById<Button>(R.id.HomeInventoryButton)
-        var shoppingButton = findViewById<Button>(R.id.HomeShoppingButton)
-        var storeButton = findViewById<Button>(R.id.HomeStoresButton)
-
-        inventoryButton.setOnClickListener{
-            val intent = Intent(this@HomeActivity, InventoryActivity::class.java)
-            startActivity(intent)
-        }
-
-        shoppingButton.setOnClickListener{
-            val intent = Intent(this@HomeActivity, ShoppingActivity::class.java)
-            startActivity(intent)
-        }
-
-        storeButton.setOnClickListener{
-            val intent = Intent(this@HomeActivity, StoreListActivity::class.java)
-            startActivity(intent)
-        }
+        var toolbar : Toolbar? = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 
 }
